2835919136288662706bc600a1d37d6bb0017fc3
rxuriguera <rxuriguera@09908f4e-1ebf-7930-00f3-edde6cedbad1>
2010-04-28 23:57:24 +0000

    Updated Serializer

--- trunk/src/speerker/util/Serializer.java
+++ trunk/src/speerker/util/Serializer.java
@@ -1,3 +1,21 @@
+/*
+ * Speerker 
+ * Copyright (C) 2010  Jordi Bartrolí, Hector Mañosas i Ramon Xuriguera
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package speerker.util;
 
 import java.io.ByteArrayOutputStream;
@@ -15,22 +33,32 @@ public class Serializer {
 	 *            A serializable object
 	 * @return A byte array
 	 */
-	public static byte[] serialize(Serializable object) throws IOException{
+	public static byte[] serialize(Serializable object) {
 		byte[] sObject = "".getBytes();
 		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-		ObjectOutputStream out = new ObjectOutputStream(bOut);
-		out.writeObject(object);
+		ObjectOutputStream out;
+		try {
+			out = new ObjectOutputStream(bOut);
+			out.writeObject(object);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		sObject = bOut.toByteArray();
 		return sObject;
 	}
 
-	public static Serializable deserialize(byte[] sObject) throws IOException, ClassNotFoundException {
+	public static Serializable deserialize(byte[] sObject)
+			throws ClassNotFoundException {
 		Object object = null;
 
 		ByteArrayInputStream bIn = new ByteArrayInputStream(sObject);
-		ObjectInputStream in = new ObjectInputStream(bIn);
-		object = in.readObject();
-
+		ObjectInputStream in;
+		try {
+			in = new ObjectInputStream(bIn);
+			object = in.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return (Serializable) object;
 	}
 }
