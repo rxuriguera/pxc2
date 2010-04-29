4ddf3f6061128f17f9accab3b68efff0b861d57b
rxuriguera <rxuriguera@09908f4e-1ebf-7930-00f3-edde6cedbad1>
2010-04-29 12:03:08 +0000

    File Appender

---
+++ src-tests/speerker/FileAppender.java
@@ -0,0 +1,36 @@
+package speerker;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileAppender {
+	public static void main(String argv[]) throws InterruptedException, IOException {
+		String inputPath = "/home/rxuriguera/enlistments/speerker/.temp/a.mp3";
+		String outputPath = "/home/rxuriguera/enlistments/speerker/.temp/b.mp3";
+		Boolean stop = false;
+
+		FileInputStream infile = new FileInputStream(inputPath);
+		BufferedInputStream bis = new BufferedInputStream(infile);
+
+		Integer len =10000;
+		FileOutputStream outfile = new FileOutputStream(outputPath, true);
+		BufferedOutputStream buf = new BufferedOutputStream(outfile);
+		byte[] filedata = null;
+
+		while (stop == false) {
+			filedata = new byte[len];
+			bis.read(filedata);
+			buf.write(filedata);
+			Thread.sleep(1000);
+		}
+
+		bis.close();
+		infile.close();
+
+		buf.close();
+		outfile.close();
+	}
+}
