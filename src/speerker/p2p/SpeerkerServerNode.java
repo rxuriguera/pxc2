2adbe1f26df13feef553f670ef8dd677f26eece1
rxuriguera <rxuriguera@09908f4e-1ebf-7930-00f3-edde6cedbad1>
2010-05-05 08:05:24 +0000

    Added legal notice

--- src/speerker/p2p/SpeerkerServerNode.java
+++ src/speerker/p2p/SpeerkerServerNode.java
@@ -1,3 +1,21 @@
+/*
+ * Speerker 
+ * Copyright (C) 2010  Jordi Bartrolí, Hector Mañosas i Ramon Xuriguera
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package speerker.p2p;
 
 import peerbase.Node;
@@ -33,11 +51,13 @@ public class SpeerkerServerNode extends Node {
 	public static void main(String[] args) {
 		String serverHost = App.getProperty("ServerHost");
 		Integer serverPort = Integer.valueOf(App.getProperty("ServerPort"));
-		PeerInfo serverInfo = new PeerInfo("SpeerkerServer", serverHost, serverPort);
+		PeerInfo serverInfo = new PeerInfo("SpeerkerServer", serverHost,
+				serverPort);
 		Integer maxPeers = Integer.valueOf(App.getProperty("ServerMaxPeers"));
-		
-		final SpeerkerServerNode peer = new SpeerkerServerNode(serverInfo, maxPeers);
-		
+
+		final SpeerkerServerNode peer = new SpeerkerServerNode(serverInfo,
+				maxPeers);
+
 		// Start server main loop
 		Thread peerThread = (new Thread() {
 			public void run() {
@@ -46,7 +66,8 @@ public class SpeerkerServerNode extends Node {
 			}
 		});
 		peerThread.start();
-		App.logger.info("Server peer on: "+serverInfo.toString()+" started.");
+		App.logger.info("Server peer on: " + serverInfo.toString()
+				+ " started.");
 
 		peer.startStabilizer(new SimplePingStabilizer(peer), 10000);
 	}
